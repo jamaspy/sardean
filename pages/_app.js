a17516c40a841d54c196b41d7cd294e17a8c87d7
jamaspy <aspin@me.com>
2023-01-17 16:11:15 +1100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Space_Grotesk } from "@next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 const space = Space_Grotesk({
   subsets: ["latin"],
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <main className={`${space.variable} font-sans`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
